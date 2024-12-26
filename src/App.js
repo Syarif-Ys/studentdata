5e83672c48340d59c35d1469ddb360a92c0279f9
Syarif-Ys <5yarif.y.s@gmail.com>
2024-12-26 15:28:34 +0800

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+);
+}
+
+export default App;

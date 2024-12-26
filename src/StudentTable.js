e5d9d281401d08df9189f50c7a81910329b8d1f6
Syarif-Ys <5yarif.y.s@gmail.com>
2024-12-26 20:34:03 +0800

    add Link

--- src/StudentTable.js
+++ src/StudentTable.js
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom";
+
 export default function StudentTable(){
     return (
         <div className="container">
             <h2>Student Records</h2>
+            <div className="table container">
+                <Link to="/student/create" class="btn btn-add">Add new Student</Link>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Place</th>
+                            <th>Phone</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Khanam</td>
+                            <td>Bengaluru</td>
+                            <td>1234567</td>
+                            <td><a href="" className="btn btn-info">View</a>
+                                <a href="" className="btn btn-primary">Edit</a>
+                                <a href="" className="btn btn-danger">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
